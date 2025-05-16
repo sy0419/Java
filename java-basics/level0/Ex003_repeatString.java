8f91ede198fba18485bc34ae219dace95ee5d725
sy0419 <psy041912@gmail.com>
2025-05-17 02:28:07 +0900

    Print the string 'str' as 'n' times.

--- java-basics/level0/Ex003_repeatString.java
+++ java-basics/level0/Ex003_repeatString.java
@@ -1,7 +1,7 @@
 /* 문자열 str과 정수 n이 주어집니다. str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
 제한사항) 1 ≤ str의 길이 ≤ 10 , 1 ≤ n ≤ 5 */
 
-public class repeatString_003 {
+public class Ex003_repeatString {
     public static void main(String[] args) {
         String result = solution("string", 5);
         System.out.println(result);
