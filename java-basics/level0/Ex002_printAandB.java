266ea4e56231e1cb5a8701626626c9ee2709b273
sy0419 <psy041912@gmail.com>
2025-05-17 02:27:40 +0900

    Print integer values 'a' and 'b'.

--- java-basics/level0/Ex002_printAandB.java
+++ java-basics/level0/Ex002_printAandB.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class printAandB_002 {
+public class Ex002_printAandB {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
