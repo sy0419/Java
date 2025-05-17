c62ad644af8e8206ea5246db6fb1058d4395ffa6
sy0419 <psy041912@gmail.com>
2025-05-18 02:02:00 +0900

    Print the string.

--- java-basics/level0/Ex001_printString.java
+++ java-basics/level0/Ex001_printString.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 
 public class Ex001_printString {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        System.out.println(a);
+        try (Scanner sc = new Scanner(System.in)) {
+            String a = sc.next();
+            System.out.println(a);
+        }
     }
 }
