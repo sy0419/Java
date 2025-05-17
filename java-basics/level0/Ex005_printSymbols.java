2f2673a5da0a8bf0ac10eb3ccc053c61504a5c1b
sy0419 <psy041912@gmail.com>
2025-05-18 02:03:15 +0900

    Print symbols.

---
+++ java-basics/level0/Ex005_printSymbols.java
@@ -0,0 +1,7 @@
+/* "!@#$%^&*(\'"<>?:;" 출력하도록 코드를 작성해 주세요. */
+
+public class Ex005_printSymbols {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
