6b5e6929e930d3e05e81d5e0aebec8353314028a
sy0419 <psy041912@gmail.com>
2025-05-31 22:39:57 +0900

    Print the string that corresponds to the number array.

--- java-basics/level0/Ex027_manipulateNumber2.java
+++ java-basics/level0/Ex027_manipulateNumber2.java
@@ -1,8 +1,3 @@
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 /* 정수 배열 numLog가 주어집니다. 처음에 numLog[0]에서 부터 시작해 "w", "a", "s", "d"로 이루어진 문자열을 입력으로 받아 순서대로 다음과 같은 조작을 했다고 합시다.
    "w" : 수에 1을 더한다. "s" : 수에 1을 뺀다.
    "d" : 수에 10을 더한다. "a" : 수에 10을 뺀다. 
@@ -11,6 +6,10 @@ import java.util.Scanner;
    주어진 정수 배열 numLog에 대해 조작을 위해 입력받은 문자열을 return 하는 solution 함수를 완성해 주세요.
    제한사항) 2 ≤ numLog의 길이 ≤ 100,000 , -100,000 ≤ numLog[0] ≤ 100,000 
             1 ≤ i ≤ numLog의 길이인 모든 i에 대해 |numLog[i] - numLog[i - 1]|의 값은 1 또는 10입니다. */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Ex027_manipulateNumber2 {
     public static void main(String[] args) {
