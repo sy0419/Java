fd3680fc96653f7eb2712447d77072ec890b07a8
sy0419 <psy041912@gmail.com>
2025-09-03 18:00:34 +0900

    Implement two methods to return sublist from n-th element to end (loop & optimized).

--- java-basics/level0/Ex059_arrayFromN.java
+++ java-basics/level0/Ex059_arrayFromN.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 // 정수 리스트 num_list와 정수 n이 주어질 때, 
 // n 번째 원소부터 마지막 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
 
@@ -8,6 +5,9 @@ import java.util.Arrays;
 // 2 ≤ num_list의 길이 ≤ 30
 // 1 ≤ num_list의 원소 ≤ 9
 // 1 ≤ n ≤ num_list의 길이
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Ex059_arrayFromN {
     public static void main(String[] args) {
