162face6d7b182da5edcb04cf824b33ec2be2f8d
sy0419 <psy041912@gmail.com>
2025-09-04 18:36:47 +0900

    .

---
+++ java-basics/level0/Ex063_intervalsN.java
@@ -0,0 +1,16 @@
+// 정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 마지막 원소까지 n개 간격으로 저장되어있는 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 5 ≤ num_list의 길이 ≤ 20
+// 1 ≤ num_list의 원소 ≤ 9
+// 1 ≤ n ≤ 4
+
+public class Ex063_intervalsN {
+    public static void main(String[] args) {
+        System.out.println(solution());
+    }
+
+    public static int[] solution(int[] numList, int n){
+        
+    }
+}
