c61dacb2bbae64dddd49e5a7c9dc114e13996fdf
sy0419 <psy041912@gmail.com>
2025-09-05 18:13:53 +0900

    .

---
+++ java-basics/level0/Ex078_changeSpecificLettersToUppercase.java
@@ -0,0 +1,15 @@
+// 영소문자로 이루어진 문자열 my_string과 영소문자 1글자로 이루어진 문자열 alp가 매개변수로 주어질 때, 
+// my_string에서 alp에 해당하는 모든 글자를 대문자로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// 1 ≤ my_string의 길이 ≤ 1,000
+
+public class Ex078_changeSpecificLettersToUppercase {
+    public static void main(String[] args) {
+        System.out.println(solution());
+    }
+
+    public static String solution(String myString, Strign alp) {
+        
+    }
+}
