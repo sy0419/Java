58d26b68104cf2434ffc70329db4d1e121d8a42c
sy0419 <psy041912@gmail.com>
2025-09-05 18:52:58 +0900

    .

---
+++ java-basics/level0/Ex079_endWithTheSpecificLetters.java
@@ -0,0 +1,18 @@
+// 문자열 myString과 pat가 주어집니다. myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
+
+// 제한사항
+// 5 ≤ myString ≤ 20
+// 1 ≤ pat ≤ 5
+// pat은 반드시 myString의 부분 문자열로 주어집니다.
+// myString과 pat에 등장하는 알파벳은 대문자와 소문자를 구분합니다.
+
+public class Ex079_endWithTheSpecificLetters {
+    public static void main(String[] args) {
+        System.out.println(solution("AbCdEFG", "dE"));
+        System.out.println(solution("AAAAaaaa", "a"));
+    }
+
+    public static String solution(String myString, String pat) {
+        
+    }
+}
