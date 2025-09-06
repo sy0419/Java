c2bdc3acc99f00fbab130d2c22a57dc38ecb469d
sy0419 <psy041912@gmail.com>
2025-09-06 16:14:42 +0900

    Implement character trick by replacing 'm' with 'rn'.

---
+++ java-basics/level0/Ex088_stringTrick.java
@@ -0,0 +1,17 @@
+// 'm'과 "rn"이 모양이 비슷하게 생긴 점을 활용해 문자열에 장난을 하려고 합니다. 문자열 rny_string이 주어질 때, rny_string의 모든 'm'을 "rn"으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// 1 ≤ rny_string의 길이 ≤ 100
+// rny_string은 영소문자로만 이루어져 있습니다.
+public class Ex088_stringTrick {
+    public static void main(String[] args) {
+        System.out.println(solution("masterpiece"));
+        System.out.println(solution("programmers"));
+        System.out.println(solution("jerry"));
+        System.out.println(solution("burn"));
+    }
+
+    public static String solution(String rnyString) {
+        return rnyString.replace("m", "rn");
+    }
+}
