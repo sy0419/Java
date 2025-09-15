355bcde651bd84640e2c3f3df47b680e916e8530
sy0419 <psy041912@gmail.com>
2025-09-15 16:50:48 +0900

    Implement grouping strings by length and finding max group size.

--- java-basics/level0/Ex096_maxiumStringInArray.java
+++ java-basics/level0/Ex096_maxiumStringInArray.java
@@ -11,27 +11,32 @@ import java.util.Map;
 
 public class Ex096_maxiumStringInArray {
     public static void main(String[] args) {
-        System.out.println(solution(new String[] {"a","bc","d","efg","hi"}));
+        // 테스트 케이스 출력 # Print test case result
+        System.out.println(solution(new String[] {"a", "bc", "d", "efg", "hi"})); // 출력: 2
     }
 
     public static int solution(String[] strArr) {
+        // 문자열 길이를 key로, 해당 길이의 문자열 개수를 value로 저장할 맵 생성 # Create a map with string length as key and count as value
         Map<Integer, Integer> map = new HashMap<>();
 
+        // 각 문자열의 길이를 기준으로 개수 세기 # Count strings grouped by their length
         for (String s : strArr) {
             int len = s.length();
-
             if (map.containsKey(len)) {
                 map.put(len, map.get(len) + 1);
             } else {
                 map.put(len, 1);
             }
         }
+
+        // 가장 많은 개수를 가진 그룹의 크기 찾기 # Find the size of the largest group
         int max = 0;
         for (int count : map.values()) {
             if (count > max) {
                 max = count;
             }
         }
+
         return max;
     }
 }
