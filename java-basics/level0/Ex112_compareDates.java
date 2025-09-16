e957b1133ffa8762f3d55378175953a259ae2bb3
sy0419 <psy041912@gmail.com>
2025-09-16 16:20:00 +0900

    Implement date comparison using manual and LocalDate methods.

--- java-basics/level0/Ex112_compareDates.java
+++ java-basics/level0/Ex112_compareDates.java
@@ -12,13 +12,16 @@ import java.time.LocalDate;
 
 public class Ex112_compareDates {
     public static void main(String[] args) {
+        // 테스트 케이스 출력 # Print test cases
         System.out.println(solution1(new int[] {2021, 12, 28}, new int[] {2021, 12, 29}));
         System.out.println(solution1(new int[] {1024, 10, 24}, new int[] {1024, 10, 24}));
         System.out.println(solution2(new int[] {2021, 12, 28}, new int[] {2021, 12, 29}));
         System.out.println(solution2(new int[] {1024, 10, 24}, new int[] {1024, 10, 24}));
     }
 
+    // 날짜를 직접 요소별로 비교하는 방식 # Compare year, month, and day manually
     public static int solution1(int[] date1, int[] date2) {
+        // 연도 비교 # Compare year
         if (date1[0] < date2[0]) return 1;
         if (date1[0] > date2[0]) return 0;
 
@@ -28,13 +31,16 @@ public class Ex112_compareDates {
 
         // 일 비교 # Compare day
         if (date1[2] < date2[2]) return 1;
-        else return 0;
+        else return 0;  // 같거나 이후인 경우 # Same or after
     }
 
+    // 자바의 LocalDate 클래스를 활용하는 방식 # Use Java's LocalDate class
     public static int solution2(int[] date1, int[] date2) {
+        // LocalDate 인스턴스 생성 # Create LocalDate instances
         LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
         LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
 
+        // dateA가 dateB보다 앞서는지 확인 # Check if dateA is before dateB
         if (dateA.isBefore(dateB)) {
             return 1;
         } else {
