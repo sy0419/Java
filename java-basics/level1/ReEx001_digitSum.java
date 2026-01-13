7e50d51bd54eced116316756bb90c594e1ea906b
sy0419 <psy041912@gmail.com>
2026-01-13 18:57:16 +0900

    Implement digit sum using both string and arithmetic approaches.

---
+++ java-basics/level1/ReEx001_digitSum.java
@@ -0,0 +1,15 @@
+public class ReEx001_digitSum {
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+        System.out.println(solution(987));
+    }
+
+    public static int solution(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n = n / 10;
+        }
+        return sum;
+    }
+}
