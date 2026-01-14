5557d283fcaafe2213268b9ad66269def4ce5874
sy0419 <psy041912@gmail.com>
2026-01-14 18:21:49 +0900

    Implement reversed digits extraction for long input.

--- java-basics/level1/ReEx002_reverseDigits.java
+++ java-basics/level1/ReEx002_reverseDigits.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ReEx002_reverseDigits {
     public static void main(String[] args) {
-        System.out.println(solution(12345));
+        System.out.println(Arrays.toString(solution(12345)));
     }
 
-    public static int[] solution(int num) {
-        
+    public static int[] solution(long n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        String strNum = Long.toString(n);
+        for (int i = strNum.length() - 1; i >= 0; i--) {
+            char ch = strNum.charAt(i);
+            arr.add(ch - '0');
+        }
+
+        int[] result = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            result[i] = arr.get(i);
+        }
+        return result;
     }
 }
