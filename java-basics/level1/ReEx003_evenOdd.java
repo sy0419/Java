b67c8bc259c14dfecf1f1cc53e2499d281f05e82
sy0419 <psy041912@gmail.com>
2026-01-14 17:38:24 +0900

    Implement reversed digits extraction for long input.

---
+++ java-basics/level1/ReEx003_evenOdd.java
@@ -0,0 +1,16 @@
+public class ReEx003_evenOdd {
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+    }
+    
+    public static String solution(int num) {
+        String result = "";
+        if (num % 2 == 1) {
+            result += "Odd";
+        } else {
+            result += "Even";
+        }
+        return result;
+    }
+}
