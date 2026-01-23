6c9dc860709251146270976fcbef483bff46ddb3
sy0419 <psy041912@gmail.com>
2026-01-23 17:16:40 +0900

    Implement character counter to compare number of 'p' and 'y'.

---
+++ java-basics/level1/ReEx012_CountPY.java
@@ -0,0 +1,23 @@
+public class ReEx012_CountPY {
+    public static void main(String[] args) {
+        System.out.println(solution("pPoooyY"));
+        System.out.println(solution("Pyy"));
+    }
+
+    public static boolean solution(String s) {
+        String intergrates = s.toLowerCase();
+        int pCount = 0;
+        int yCount = 0;
+
+        for (int i = 0; i < intergrates.length(); i++) {
+            char ch = intergrates.charAt(i);
+            if (ch == 'p') {
+                pCount++;
+            } else if (ch == 'y') {
+                yCount++;
+            } 
+        }
+
+        return pCount == yCount;
+    }
+}
