63b0c88bd9fb974e639d09032399eb14a9d43ba8
sy0419 <psy041912@gmail.com>
2026-01-23 17:55:33 +0900

    Implement solution to find Kim's position in an array.

---
+++ java-basics/level1/ReEx017_FindKim.java
@@ -0,0 +1,16 @@
+public class ReEx017_FindKim {
+    public static void main(String[] args) {
+        System.out.println(solution(new String[] {"Jane", "Kim"}));
+    }
+    
+    public static String solution(String[] seoul) {
+        int index = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                index = i;
+            }
+        }
+
+        return String.format("Kim is in %d.", index);
+    }
+}
