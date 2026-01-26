55efc2fb927db016d133bb7038c467ab4b49039f
sy0419 <psy041912@gmail.com>
2026-01-26 17:34:51 +0900

    Implement phone number masking with two methods using StringBuilder and char array.

--- java-basics/level1/Ex019_HidePhoneNumber.java
+++ java-basics/level1/Ex019_HidePhoneNumber.java
@@ -1,10 +1,10 @@
-// 프로그래머스 모바일은 개인Ex019_HidePhoneNumber정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다. 
+// 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다. 
 // 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
 
 // 제한 조건
 // phone_number는 길이 4 이상, 20이하인 문자열입니다.
 
-public class  {
+public class Ex019_HidePhoneNumber {
     public static void main(String[] args) {
         System.out.println(solution1("01033334444"));  // *******4444 출력 # prints *******4444
         System.out.println(solution1("027778888"));    // *****8888 출력 # prints *****8888
