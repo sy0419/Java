4694e94479a9f9eba1b30e7d2ee23b04bfbf9c5c
sy0419 <psy041912@gmail.com>
2026-01-26 18:19:17 +0900

    Implement dot product solution for two integer arrays.

---
+++ java-basics/level1/ReEx022_DotProduct.java
@@ -0,0 +1,16 @@
+public class ReEx022_DotProduct {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {1, 2, 3, 4}, new int[] {-3, -1, 0, 2}));
+        System.out.println(solution(new int[] {-1, 0, 1}, new int[] {1, 0, -1}));
+    }
+
+    public static int solution (int[] a, int[] b) {
+        int result = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            result += a[i] * b[i];
+        }
+
+        return result;
+    }
+}
