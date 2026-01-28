5f9addf7f1b6f085fd6b367c24f7608862da2059
sy0419 <psy041912@gmail.com>
2026-01-28 17:14:15 +0900

    Implement solution to reverse a number in ternary and convert back to decimal.

--- java-basics/level1/ReEx034_reverseTernary.java
+++ java-basics/level1/ReEx034_reverseTernary.java
@@ -1,3 +1,15 @@
 public class ReEx034_reverseTernary {
+    public static void main(String[] args) {
+        System.out.println(solution(45));
+        System.out.println(solution(125));
+    }
 
+    public static int solution(int n) {
+        String nStr = Integer.toString(n, 3);
+        String reversed = new StringBuilder(nStr).reverse().toString();
+
+        int result = Integer.parseInt(reversed, 3);
+
+        return result;
+    }
 }
