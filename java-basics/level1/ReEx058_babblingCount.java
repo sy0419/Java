f8c987805185b3d3f7473c44241416d88cc7e073
sy0419 <psy041912@gmail.com>
2026-02-13 18:46:40 +0900

    Implement babbling word count with consecutive repetition check.

---
+++ java-basics/level1/ReEx058_babblingCount.java
@@ -0,0 +1,36 @@
+
+
+public class ReEx058_babblingCount {
+    public static void main(String[] args) {
+        System.out.println(solution(new String[] {"aya", "yee", "u", "maa"}));
+        System.out.println(solution(new String[] {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"}));
+    }
+
+    public static int solution(String[] babbiling) {
+        int result = 0;
+        String[] sounds = {"aya", "ye", "woo", "ma"};
+
+        for (String word : babbiling) {
+            String last = "";
+            while (!word.isEmpty()) {
+                boolean found = false;
+
+                for (String sound: sounds) {
+                    if (word.startsWith(sound) && sound != last) {
+                        word = word.substring(sound.length());
+                        last = sound;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    break;
+                }
+            } if (word.isEmpty()) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
