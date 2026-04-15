b1864bd25e4313f4683919f4466c07312a0961ba
sy0419 <psy041912@gmail.com>
2026-04-15 17:28:05 +0900

    Implement robot dog park walk simulation with obstacle and boundary checks.

---
+++ java-basics/level1/ReEx080_RobotDogParkWalk.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class ReEx080_RobotDogParkWalk {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new String[] {"SOO","OOO","OOO"}, new String[] {"E 2","S 2","W 1"})));
+        System.out.println(Arrays.toString(solution(new String[] {"SOO","OXX","OOO"}, new String[] {"E 2","S 2","W 1"})));
+        System.out.println(Arrays.toString(solution(new String[] {"OSO","OOO","OXO","OOO"}, new String[] {"E 2","S 3","W 1"}))); 
+    }
+
+    public static int[] solution(String[] park, String[] routes) {
+        int row = 0;
+        int col = 0;
+        boolean sFind = false;
+
+        for (int i = 0; i < park.length; i++) {
+            for (int j = 0; j < park[i].length(); j++) {
+                if (park[i].charAt(j) == 'S') {
+                    row = i;
+                    col = j;
+                    sFind = true;
+                    break;
+                }
+            }
+            if (sFind) {
+                break;
+            }
+        }
+
+        for (String route: routes) {
+            String[] parts = route.split(" ");
+
+            String direction = parts[0];
+            int distance = Integer.parseInt(parts[1]);
+
+            int dr = 0;
+            int dc = 0;
+            
+            if (direction.equals("N")) {
+                dr = -1;
+            } else if (direction.equals("S")) {
+                dr = +1;
+            } else if (direction.equals("W")) {
+                dc = -1;
+            } else {
+                dc = +1;
+            }
+
+            int nr = row;
+            int nc = col;
+            boolean canMove = true;
+            for (int i = 0; i < distance; i++) {
+                nr += dr;
+                nc += dc;
+                if (nr < 0 || nr >= park.length || nc < 0 || nc >= park[0].length()) {
+                    canMove = false;
+                    break;
+                }
+
+                if (park[nr].charAt(nc) == 'X') {
+                    canMove = false;
+                    break;
+                }
+            }
+
+            if (canMove) {
+                row = nr;
+                col = nc;
+            }
+        }
+        return new int[] {row, col};
+    }
+}
