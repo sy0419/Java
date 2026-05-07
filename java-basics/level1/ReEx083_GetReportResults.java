02321587753bd0b9868f602bbbf256166039b7e8
sy0419 <psy041912@gmail.com>
2026-05-07 16:44:32 +0900

    Implement report system to count banned users and send notification mails.

---
+++ java-basics/level1/ReEx083_GetReportResults.java
@@ -0,0 +1,12 @@
+public class ReEx083_GetReportResults {
+    public static void main(String[] args) {
+        System.out.println(solution(new String[] {"muzi", "frodo", "apeach", "neo"},
+                new String[] {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"}, 2));
+        System.out.println(solution(new String[] {"con", "ryan"},
+                new String[] {"ryan con", "ryan con", "ryan con", "ryan con"}, 3));
+    }
+
+    public static int[] solution(String[] id_list, String[] report, int k) {
+
+    }
+}
