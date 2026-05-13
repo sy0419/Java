523e0f6ac14c5e0f5e18df926bd6f3277aed101b
sy0419 <psy041912@gmail.com>
2026-05-13 18:07:53 +0900

    Implements Fibonacci sequence calculation using dynamic programming with modulo optimization.

--- java-basics/level2/Ex008_FibonacciNumber.java
+++ java-basics/level2/Ex008_FibonacciNumber.java
@@ -22,31 +22,26 @@
 // 입출력 예 설명
 // 피보나치수는 0번째부터 0, 1, 1, 2, 3, 5, ... 와 같이 이어집니다.
 
-public class Ex008_FibonacciNumber {
-    public static void main(String[] args) {
-        System.out.println(solution(3));
-        System.out.println(solution(5));
-    }
+class Solution {
 
-    public static int solution(int n) {
+    public int solution(int n) {
 
         // 피보나치 수를 저장할 배열 생성 # Create array to store Fibonacci numbers
-        int[] fb = new int[n+1];
+        int[] fb = new int[n + 1];
 
         // 피보나치 초기값 설정 # Set initial Fibonacci values
         fb[0] = 0;
         fb[1] = 1;
 
-        // 2번째 피보나치 수부터 계산 # Calculate Fibonacci numbers starting from index 2
+        // 2번째 피보나치 수부터 반복 계산 # Calculate Fibonacci numbers starting from index 2
         for (int i = 2; i <= n; i++) {
 
-            // 이전 두 값을 더하여 현재 피보나치 값 저장
-            // Store current Fibonacci value using previous two values
-            fb[i] = fb[i-1] + fb[i-2];
+            // 이전 두 값을 더한 뒤 나머지 저장
+            // Store modulo value after adding previous two Fibonacci numbers
+            fb[i] = (fb[i - 1] + fb[i - 2]) % 1234567;
         }
 
-        // n번째 피보나치 수를 1234567로 나눈 나머지 반환
-        // Return Fibonacci number modulo 1234567
-        return fb[n] % 1234567;
+        // n번째 피보나치 수 반환 # Return nth Fibonacci number
+        return fb[n];
     }
 }
